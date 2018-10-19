d839ff5126a678819385c9ee1d129040d699e508
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-10-19 06:53:10 -0700

    Update to 1.11.2a streamsx.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'streamsx.testing',
   packages = ['streamsx.testing'],
   include_package_data=True,
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'IBM Streams tester',
   long_description = open('DESC.txt').read(),
   author = 'IBM Streams @ github.com',
@@ -17,7 +17,7 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
-  #install_requires=['streamsx==1.11.1a', 'nose'],
+  #install_requires=['streamsx==1.11.2a', 'nose'],
   entry_points = {
     'nose.plugins.0.10': [
     'streamsx-jco = streamsx.testing.nose:JobConfigPlugin',
